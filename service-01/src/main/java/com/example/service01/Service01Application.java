b498abb0ae0a15298c3559d4d1018b10d65428de
Mengzhen-Zong <mengzhen_zong@outlook.com>
2023-10-14 18:48:20 +0800

    build: configure config server in service-01 module

--- service-01/src/main/java/com/example/service01/Service01Application.java
+++ service-01/src/main/java/com/example/service01/Service01Application.java
@@ -1,5 +1,6 @@
 package com.example.service01;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -11,12 +12,19 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class Service01Application {
 
+    @Value("${output}")
+    private String output;
+
     @GetMapping("/")
     public String home() {
-        return "hello world";
+        return output;
     }
 
     public static void main(String[] args) {
         SpringApplication.run(Service01Application.class, args);
+    }
+
+    public void setOutput(String output) {
+        this.output = output;
     }
 }
